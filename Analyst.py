e0438b4e68026cb3d3980f49fddd95852f4a5dfb
ChrisJobs046 <ChrisJobs046@gmail.com>
2023-07-03 09:44:34 -0400

    starting data analysis

---
+++ Analyst.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+Dataset = os.getenv("Airbnb")
+
